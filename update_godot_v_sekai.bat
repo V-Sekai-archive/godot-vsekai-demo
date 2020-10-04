cec65020a08859fae83b518bd7ac5e5c680599cc
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2020-10-03 22:59:27 -0700

    Create update_godot_v_sekai.bat

---
+++ update_godot_v_sekai.bat
@@ -0,0 +1,30 @@
+# Checkout remotes
+git remote rm SaracenOne
+git remote rm lyuma
+git remote add SaracenOne https://github.com/SaracenOne/godot
+git remote add lyuma https://github.com/lyuma/godot
+git fetch SaracenOne
+git fetch lyuma
+
+# Work
+git checkout master
+git branch -D groups
+git checkout 3.2
+git pull
+git merge remotes/SaracenOne/fire_skeleton_inspector
+git merge remotes/SaracenOne/colshape_race_cond_fix
+git merge remotes/SaracenOne/scene_tree_improvements
+git merge remotes/SaracenOne/filesystem_dock_resource_converter
+git merge remotes/SaracenOne/audio_effect_stream
+git merge remotes/SaracenOne/better_bullet
+git merge remotes/SaracenOne/gizmo_improvements
+git merge remotes/SaracenOne/rpc_mode_get
+git merge remotes/SaracenOne/require_shader
+git merge remotes/SaracenOne/renik
+git merge remotes/SaracenOne/main_scene
+git merge remotes/SaracenOne/threaddesc
+git merge remotes/SaracenOne/pool_range_ops
+git merge remotes/lyuma/non_normalized_weight_fixes
+git merge remotes/SaracenOne/runtime_wave_loader
+git merge remotes/SaracenOne/load_interactive_no_cache
+rem git merge editor_plugin_node_ownership
