1296c81d59705a15855b22d5214fd59d9b06ad4f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-04 00:58:32 -0700

    Wrong url.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -12,7 +12,7 @@ git remote rm godotengine
 git remote add godotengine https://github.com/godotengine/godot
 git fetch godotengine
 git remote rm v-sekai-godot
-git remote add v-sekai-godot git@github.com:SaracenOne/godot.git
+git remote add v-sekai-godot git@github.com:v-sekai/godot.git
 git fetch v-sekai-godot
 
 echo -e "Work"
