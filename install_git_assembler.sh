5a83e21c139c13b9fa244310e67374337082d75b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-04 00:43:34 -0700

    Correct assembler install.

--- install_git_assembler.sh
+++ install_git_assembler.sh
@@ -1,2 +1,3 @@
 sudo curl -L https://gitlab.com/wavexx/git-assembler/-/raw/master/git-assembler -o /usr/local/bin/git-assembler
+sudo chmod +x /usr/local/bin/git-assembler
 git config --global alias.as assembler
