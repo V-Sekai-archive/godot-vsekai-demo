449dded08e1173e309e01ca51a722682641d43c5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-09 12:44:08 -0700

    Ignore return.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -4,22 +4,22 @@ echo -e "Checkout remotes"
 git remote rm SaracenOne
 git remote add SaracenOne https://github.com/SaracenOne/godot
 git fetch SaracenOne
-git remote rm lyuma
+git remote rm lyuma || true
 git remote add lyuma https://github.com/lyuma/godot
 git fetch lyuma
-git remote rm godot-fire
+git remote rm godot-fire || true
 git remote add godot-fire https://github.com/godot-extended-libraries/godot-fire
 git fetch godot-fire
 git remote rm godotengine
 git remote add godotengine https://github.com/godotengine/godot
 git fetch godotengine
-git remote rm v-sekai-godot
+git remote rm v-sekai-godot || true
 git remote add v-sekai-godot git@github.com:v-sekai/godot.git
 git fetch v-sekai-godot
 
 echo -e "Work"
 git checkout master --force
-git branch -D groups
+git branch -D groups || true
 git as -av
 git checkout groups -f
 git push -u -f v-sekai-godot groups
