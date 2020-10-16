10bd071b688fe4844e2bc7e33d5a92cd343a6c85
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-16 16:15:29 -0700

    Use groups for the generic software.

--- godot_v_sekai_update.sh
+++ godot_v_sekai_update.sh
@@ -40,7 +40,7 @@ git branch -D groups || true
 python3 ./thirdparty/git-assembler -av
 git checkout groups -f
 export MERGE_DATE=$(date --iso=sec --utc)
-export MERGE_TAG=$(echo v-sekai.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
+export MERGE_TAG=$(echo groups.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
 git merge -s ours remotes/v-sekai-godot/groups -m "Commited at $MERGE_DATE."
 git tag -a $MERGE_TAG -m "Commited at $MERGE_DATE."
 git push v-sekai-godot $MERGE_TAG
