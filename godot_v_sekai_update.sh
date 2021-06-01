bd6fccf4dd12b5d20caedc327249c0822c8397a2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-01 11:05:17 -0700

    Revert "Merge branch 'merge-script-master' into old"
    This reverts commit ddba01819f87261c5bb6a3ffb0aec9db4fadfa73.

--- godot_v_sekai_update.sh
+++ godot_v_sekai_update.sh
@@ -11,10 +11,10 @@ git remote rm lyuma || true
 git remote add lyuma https://github.com/lyuma/godot
 git remote set-url --push lyuma https://example.com/
 git fetch lyuma
-git remote rm godot-extended-libraries-godot-fire || true
-git remote add godot-extended-libraries-godot-fire https://github.com/godot-extended-libraries/godot-fire
+git remote rm godot-fire || true
+git remote add godot-fire https://github.com/godot-extended-libraries/godot-fire
 git remote set-url --push godot-fire https://example.com/
-git fetch godot-extended-libraries-godot-fire
+git fetch godot-fire
 git remote rm fire || true
 git remote add fire https://github.com/fire/godot
 git remote set-url --push fire https://example.com/
@@ -24,7 +24,7 @@ git remote add godotengine https://github.com/godotengine/godot
 git remote set-url --push godotengine https://example.com/
 git fetch godotengine
 git remote rm v-sekai-godot || true
-git remote add v-sekai-godot git@github.com:V-Sekai/godot.git
+git remote add v-sekai-godot git@github.com:v-sekai/godot.git
 # Need permission to push
 #git remote set-url --push v-sekai-godot https://example.com/
 git fetch v-sekai-godot
@@ -47,15 +47,15 @@ git fetch YeldhamDev
 
 echo -e "Work"
 git stash
-export ORIGINAL_BRANCH=merge-script-4.x
+export ORIGINAL_BRANCH=merge-script-master
 export MERGE_REMOTE=v-sekai-godot
-export MERGE_BRANCH=groups-4.x
+export MERGE_BRANCH=groups
 git checkout $ORIGINAL_BRANCH --force
 git branch -D $MERGE_BRANCH || true
 python3 ./thirdparty/git-assembler -av --recreate
 git checkout $MERGE_BRANCH -f
 export MERGE_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-export MERGE_TAG=$(echo groups-4.x.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
+export MERGE_TAG=$(echo groups.$MERGE_DATE | tr ':' ' ' | tr -d ' \t\n\r')
 git tag -a $MERGE_TAG -m "Commited at $MERGE_DATE."
 git push $MERGE_REMOTE $MERGE_TAG
 git push $MERGE_REMOTE $MERGE_BRANCH -f
