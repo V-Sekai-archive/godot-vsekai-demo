c6f6a23a9d45de5a76f72a8844d4994e90e534c9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-26 09:20:22 -0700

    Cleanup.

--- update_v_sekai_godot.sh
+++ update_v_sekai_godot.sh
@@ -3,14 +3,6 @@ set -e
 git stash
 
 echo -e "Checkout remotes"
-git remote rm SaracenOne || true
-git remote add SaracenOne https://github.com/SaracenOne/godot
-git remote set-url --push SaracenOne https://example.com/
-git fetch SaracenOne
-git remote rm lyuma || true
-git remote add lyuma https://github.com/lyuma/godot
-git remote set-url --push lyuma https://example.com/
-git fetch lyuma
 git remote rm godot-fire || true
 git remote add godot-fire https://github.com/godot-extended-libraries/godot-fire
 git remote set-url --push godot-fire https://example.com/
@@ -28,22 +20,6 @@ git remote add v-sekai-godot git@github.com:v-sekai/godot.git
 # Need permission to push
 #git remote set-url --push v-sekai-godot https://example.com/
 git fetch v-sekai-godot
-git remote rm TwistedTwigleg || true
-git remote add TwistedTwigleg https://github.com/TwistedTwigleg/godot.git
-git remote set-url --push TwistedTwigleg https://example.com/
-git fetch TwistedTwigleg
-git remote rm JFonS || true
-git remote add JFonS https://github.com/JFonS/godot.git
-git remote set-url --push JFonS https://example.com/
-git fetch JFonS
-git remote rm Xrayez || true
-git remote add Xrayez https://github.com/Xrayez/godot.git
-git remote set-url --push Xrayez https://example.com/
-git fetch Xrayez
-git remote rm YeldhamDev || true
-git remote add YeldhamDev https://github.com/YeldhamDev/godot.git
-git remote set-url --push YeldhamDev https://example.com/
-git fetch YeldhamDev
 
 echo -e "Work"
 git stash
